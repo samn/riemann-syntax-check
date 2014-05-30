d565721a3a40e1dfe8ca48b7d82b51b13f4a4f1b
Sam Neubardt <samneubardt@gmail.com>
2014-05-30 13:44:58 -0400

    Upgrade Riemann to 0.2.5, Clojure to 1.6.0

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject riemann-syntax-check "0.1.0"
+(defproject riemann-syntax-check "0.2.0"
   :description "A Riemann config syntax checker"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main riemann-syntax-check
+  :aot [riemann-syntax-check]
   :profiles {:dev {:dependencies [[midje "1.6.3" :scope "test"]]}}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [riemann "0.2.4"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [riemann "0.2.5"]])
